84f2276573e268ad44f6475b3be534ea41dfda25
1599982 <1599982@senati.pe>
2025-09-24 00:53:43 -0500

    Init commit

---
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import GridShape from "../../components/common/GridShape";
+import { Link } from "react-router";
+import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+      <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
+        {children}
+        <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
+          <div className="relative flex items-center justify-center z-1">
+            {/* <!-- ===== Common Grid Shape Start ===== --> */}
+            <GridShape />
+            <div className="flex flex-col items-center max-w-xs">
+              <Link to="/" className="block mb-4">
+                <img
+                  width={231}
+                  height={48}
+                  src="/images/logo/auth-logo.svg"
+                  alt="Logo"
+                />
+              </Link>
+              <p className="text-center text-gray-400 dark:text-white/60">
+                Free and Open-Source Tailwind CSS Admin Dashboard Template
+              </p>
+              <div className="w-80 h-120 mt-6 border-2 border-gray-400 rounded-lg flex items-center justify-center">
+                <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
+                  Sign-it with face
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="fixed z-50 hidden bottom-6 right-6 sm:block">
+          <ThemeTogglerTwo />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/AuthPages/SignIn.tsx
@@ -0,0 +1,17 @@
+import PageMeta from "../../components/common/PageMeta";
+import AuthLayout from "./AuthPageLayout";
+import SignInForm from "../../components/auth/SignInForm";
+
+export default function SignIn() {
+  return (
+    <>
+      <PageMeta
+        title="Yet Another Artificial Intelligence Training"
+        description="Where find in head tag?"
+      />
+      <AuthLayout>
+        <SignInForm />
+      </AuthLayout>
+    </>
+  );
+}
