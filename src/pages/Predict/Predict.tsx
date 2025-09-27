8123d7907592dc3e4f758f6a3a74ee407b9d94cc
1599982 <1599982@senati.pe>
2025-09-27 03:10:21 -0500

    Predict: snapshot-1

--- src/pages/Predict/Predict.tsx
+++ src/pages/Predict/Predict.tsx
@@ -9,9 +9,17 @@ import Badge from "../../components/ui/badge/Badge";
 type TrainingProps = {
 	type: string;
 	arr: (number | string)[];
+	op: boolean;
 };
 
-export default function Predict({type, arr}: TrainingProps) {
+export default function Predict({type, arr, op=false}: TrainingProps) {
+	const ops = {
+		"S": "+",
+		"R": "-",
+		"M": "x",
+		"D": "/",
+		"I": "="
+	};
 	let folder = "";
 
 	if (type === "Numeros") {
@@ -28,7 +36,7 @@ export default function Predict({type, arr}: TrainingProps) {
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
-        <div className="grid grid-cols-1 gap-4 md:gap-6">
+          <div className={`grid ${type === "Vocales" ? "" : "grid-cols-2"} md:gap-6`}>
 	          <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
 	            <div className="flex justify-between h-12 rounded-xl dark:bg-gray-800">
 	            	<p className="flex flex-col text-xl">Recolecte los datos necesarios<span className="text-sm font-light text-gray-500">Ponga la mano frente a la camara</span></p>
@@ -42,6 +50,32 @@ export default function Predict({type, arr}: TrainingProps) {
 	              <div className="w-full h-3 bg-gray-100 rounded-xl mt-1.5"></div>
 	            </div>
 	          </div>
+						{type !== "Vocales" && (
+							<>
+			          <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+			            <div className="flex justify-between h-12 rounded-xl dark:bg-gray-800">
+			            	<p className="flex flex-col text-xl">Recolecte los datos necesarios<span className="text-sm font-light text-gray-500">Ponga la mano frente a la camara</span></p>
+			            </div>
+
+			            <div className=" mt-5">
+			              <Badge color="success">
+			                <ArrowUpIcon />
+			                11.01%
+			              </Badge>
+			              <div className="h-3 bg-gray-100 rounded-xl mt-1.5"></div>
+			            </div>
+			          </div>
+			          <div className="flex justify-between items-center col-span-full rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 md:gap-5">
+									<p></p>
+									{type !== "Numeros" && (
+										<div className="flex md:gap-3">
+						      		<Button size="sm" variant="primary">Espacio</Button>
+						      		<Button size="sm" variant="primary">Borrar</Button>
+										</div>
+									)}
+								</div>
+							</>
+						)}
 	        </div>
 
           <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 p-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
@@ -53,6 +87,12 @@ export default function Predict({type, arr}: TrainingProps) {
 				         	<p className="text-xl font-medium text-gray-800 dark:text-white/90">{val}</p>
 				        </Button>
 				      ))}
+							{op && Object.entries(ops).map(([key, value]) => (
+								<Button className="flex flex-col" size="sm" variant="outline">
+									<img className="w-14 h-18" src={`/images/letters/${key}.png`} alt="op" />
+									<p className="text-xl font-medium text-gray-800 dark:text-white/90">{value}</p>
+				        </Button>
+							))}
             </div>
           </div>
         </div>
